6aeae8754495096b044db596fbd80793dcff9cd5
Andrew Ekstedt <andrew.ekstedt@gmail.com>
2018-01-14 17:08:55 -0800

    stub out client and server programs and start a Makefile

---
+++ src/client.cpp
@@ -0,0 +1,8 @@
+/* main entry point for the DSSE command-line client */
+
+#include <iostream>
+
+int main() {
+	std::cout << "Hello, world!\n";
+	return 0;
+}
---
+++ src/server.cpp
@@ -0,0 +1,9 @@
+/* main entry point for the server binary */
+
+//#include <DSSE.h>
+#include <iostream>
+
+int main() {
+	std::cout << "Hello, world!\n";
+	return 0;
+}
