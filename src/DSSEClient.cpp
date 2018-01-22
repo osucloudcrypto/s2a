27c53c6acc0555ea9207a363eb030da042eacb72
Andrew Ekstedt <andrew.ekstedt@gmail.com>
2018-01-22 15:23:51 -0800

    link with protobuf

--- src/DSSEClient.cpp
+++ src/DSSEClient.cpp
@@ -1,9 +1,9 @@
 #include "DSSE.h"
-//#include "dsse.pb.h"
+#include "dsse.pb.h"
 
 std::vector<DSSE::fileid_t>
 DSSE::Client::Search(std::string w) {
-	//DSSEMsg::Search msg;
-	//msg.set_k1("hi");
+	DSSE::msg::Search msg;
+	msg.set_k1("hi");
 	return std::vector<DSSE::fileid_t>{3};
 }
