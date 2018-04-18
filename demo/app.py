2eabc55a1dc17fb925744697392a8d4458fe6520
Andrew Ekstedt <andrew.ekstedt@gmail.com>
2018-04-18 13:31:47 -0700

    demo: basic search page

--- demo/app.py
+++ demo/app.py
@@ -1,11 +1,20 @@
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
 import flask
+from flask import request
 
 app = flask.Flask('dsse_demo')
 
 @app.route("/")
-def hello():
-    return u'Hello world'
+def index():
+    return flask.render_template('index.html')
 
+@app.route("/search")
+def search():
+    query = request.args.get("query", u"")
+    return query
 
 if __name__ == '__main__':
     app.run(debug=True)
