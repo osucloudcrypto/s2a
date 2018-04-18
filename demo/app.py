a46e48650ff8ed631425eed9487999c420296db8
Andrew Ekstedt <andrew.ekstedt@gmail.com>
2018-04-18 13:52:10 -0700

    demo: actually invoke dsse client

--- demo/app.py
+++ demo/app.py
@@ -5,7 +5,12 @@ from __future__ import unicode_literals
 import flask
 from flask import request
 
+import subprocess
+
 app = flask.Flask('dsse_demo')
+
+CLIENT_PATH = "../src/client"
+CLIENT_DIR = "../src"
 
 @app.route("/")
 def index():
@@ -14,7 +19,33 @@ def index():
 @app.route("/search")
 def search():
     query = request.args.get("query", u"")
-    return query
+    try:
+        results = run_client(query)
+    except subprocess.CalledProcessError as e:
+        print(e.output)
+        raise e
+    return flask.render_template("search.html", results=results)
+
+def run_client(query):
+    """perform a search for the given query.
+
+    returns a list of filenames.
+    raises subprocess.CalledProcessError on error.
+    """
+    output = subprocess.check_output(
+        [
+            CLIENT_PATH,
+            "search",
+            query,
+        ],
+        cwd=CLIENT_DIR,
+    )
+    results = []
+    for line in output.splitlines():
+        if line.startswith(query+":"):
+            filename = line.split(":", 1)[1].strip()
+            results.append(filename)
+    return results
 
 if __name__ == '__main__':
     app.run(debug=True)
